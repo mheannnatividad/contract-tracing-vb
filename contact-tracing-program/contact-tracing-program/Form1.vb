afed63ceac19058fe3fcb980ee57a862bbb298a8
mheann <maryannbernardo212@gmail.com>
2022-06-12 17:25:24 +0800

    add edit button function

--- contact-tracing-program/contact-tracing-program/Form1.vb
+++ contact-tracing-program/contact-tracing-program/Form1.vb
@@ -1,12 +1,41 @@
 ﻿Public Class Form1
+
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
         Dim studentContact As System.IO.StreamWriter
-        Dim handleFile As String = boxGivenName.Text + boxLastName.Text + ".txt"
+        Dim handleFile As String = boxGivenName.Text + "-" + boxLastName.Text + ".txt"
 
 
         If (boxLastName.Text = "") And (boxMiddleName.Text = "") And (boxLastName.Text = "") Then
             MessageBox.Show("Please enter your given, middle, and last name.")
         Else
+            If System.IO.File.Exists(handleFile) Then
+                MessageBox.Show("Student information already registered.")
+            Else
+
+                studentContact = My.Computer.FileSystem.OpenTextFileWriter(handleFile, True)
+                studentContact.WriteLine(boxLastName.Text + ", " + boxGivenName.Text + " " + boxMiddleName.Text + ".")
+                studentContact.WriteLine(boxStudentNo.Text)
+                studentContact.WriteLine(boxCourse.Text)
+                studentContact.WriteLine(boxEmail.Text)
+                studentContact.WriteLine(boxContactNo.Text)
+                studentContact.WriteLine(boxGender.Text)
+                studentContact.WriteLine(boxBirthdate.Text)
+                studentContact.WriteLine(boxHomeAdd.Text)
+                studentContact.Close()
+
+                MessageBox.Show("Text file create successfull")
+            End If
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        Dim studentContact As System.IO.StreamWriter
+        Dim handleFile As String = boxGivenName.Text + "-" + boxLastName.Text + ".txt"
+        If System.IO.File.Exists(handleFile) Then
+            System.IO.File.Delete(handleFile)
             studentContact = My.Computer.FileSystem.OpenTextFileWriter(handleFile, True)
             studentContact.WriteLine(boxLastName.Text + ", " + boxGivenName.Text + " " + boxMiddleName.Text + ".")
             studentContact.WriteLine(boxStudentNo.Text)
@@ -17,10 +46,9 @@
             studentContact.WriteLine(boxBirthdate.Text)
             studentContact.WriteLine(boxHomeAdd.Text)
             studentContact.Close()
-
-            MessageBox.Show("Text file create successfull")
+            MessageBox.Show("Text file edit successfull.")
+        Else
+            MessageBox.Show("Text file not found.")
         End If
-
-
     End Sub
 End Class
