b39ecb114cc6032f6412db40b582dab78601131b
mheann <maryannbernardo212@gmail.com>
2022-06-12 16:29:53 +0800

    add naming of label, textbox, and button

--- contact-tracing-program/contact-tracing-program/Form1.Designer.vb
+++ contact-tracing-program/contact-tracing-program/Form1.Designer.vb
@@ -60,20 +60,23 @@ Partial Class Form1
         '
         'Button1
         '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.Location = New System.Drawing.Point(353, 265)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 42)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Text = "save"
+        Me.Button1.UseVisualStyleBackColor = False
         '
         'Button2
         '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button2.Location = New System.Drawing.Point(266, 265)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 42)
         Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button1"
+        Me.Button2.Text = "edit"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'TextBox1
